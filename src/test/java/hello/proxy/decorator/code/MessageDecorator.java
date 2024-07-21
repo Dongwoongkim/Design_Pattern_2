fc0f5235f563df60a438a68520cd9f3974cf0b46
Dongwoongkim <rlaehddnd0422@naver.com>
2024-07-21 21:03:18 +0900

    feature : Decorator Pattern example

---
+++ src/test/java/hello/proxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,20 @@
+package hello.proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component{
+
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+        String decoResult = "**** " + component.operation() + " ****";
+        return decoResult;
+    }
+}
